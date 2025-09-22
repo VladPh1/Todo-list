5ee88033f1ddcb9452f3ad1291f3ea600d30cac8
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 14:54:12 +0300

    add to list/urls.py path tags create

--- list/urls.py
+++ list/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 
 from list.views import(index,
 TagsListView,
+TagsCreateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagsListView.as_view(), name="tags"),
+    path("tags/create", TagsCreateView.as_view(), name="tags-create"),
 ]
 
 app_name = "list"
