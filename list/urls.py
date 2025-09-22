a91b900eafb672b5f3e831000110c1f089ff4767
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 14:55:31 +0300

    add to list/urls.py path tags update

--- list/urls.py
+++ list/urls.py
@@ -4,12 +4,14 @@ from django.urls import path
 from list.views import(index,
 TagsListView,
 TagsCreateView,
+TagsUpdateView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagsListView.as_view(), name="tags"),
     path("tags/create", TagsCreateView.as_view(), name="tags-create"),
+    path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tags-update"),
 ]
 
 app_name = "list"
