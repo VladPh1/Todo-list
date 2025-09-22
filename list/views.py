2398b75e0a9356bf1db03cb305aaef6668106c40
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 14:09:36 +0300

    create templates/list/index.html & add num visits in index from list/views.py

--- list/views.py
+++ list/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request) -> HttpResponse:
-    return render()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "num_visits": num_visits + 1
+    }
+    return render(request, "list/index.html", context=context)
