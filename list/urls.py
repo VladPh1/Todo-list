ddd0936d9a6726ffec57ed2a8ccf6d086d065f38
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 16:57:20 +0300

    change path task list view and remove index

--- list/urls.py
+++ list/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from list.views import(index,
+from list.views import(
 TagsListView,
 TagsCreateView,
 TagsUpdateView,
@@ -13,15 +13,14 @@ TaskDeleteView,
 )
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("tags/", TagsListView.as_view(), name="tags"),
-    path("tags/create", TagsCreateView.as_view(), name="tags-create"),
-    path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tags-update"),
-    path("tags/<int:pk>/delete", TagsDeleteView.as_view(), name="tags-delete"),
-    path("task/", TaskListView.as_view(), name="task"),
+    path("", TaskListView.as_view(), name="task-list"),
     path("task/create", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagsListView.as_view(), name="tags-list"),
+    path("tags/create", TagsCreateView.as_view(), name="tags-create"),
+    path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tags-update"),
+    path("tags/<int:pk>/delete", TagsDeleteView.as_view(), name="tags-delete"),
 ]
 
 app_name = "list"
