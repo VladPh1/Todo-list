8ad9d821c27fafb2e569ab0ec7b62b02e4562c34
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 21:45:29 +0300

    fixed errors with path in list/urls.py

--- list/urls.py
+++ list/urls.py
@@ -16,12 +16,12 @@ toggle_task_status
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
     path("tags/create/", TagsCreateView.as_view(), name="tags-create"),
-    path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tags-update"),
-    path("tags/<int:pk>/delete", TagsDeleteView.as_view(), name="tags-delete"),
+    path("tags/<int:pk>/update/", TagsUpdateView.as_view(), name="tags-update"),
+    path("tags/<int:pk>/delete/", TagsDeleteView.as_view(), name="tags-delete"),
     path("task/<int:pk>/toggle-status/", toggle_task_status, name="task-toggle-status"),
 ]
 
