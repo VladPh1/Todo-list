c5b2d5c9f6a1b7b16eb05c811737b335a4f25ad3
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-22 15:00:02 +0300

    add to list/urls.py path task tasklistview & create & update & delete

--- list/urls.py
+++ list/urls.py
@@ -5,6 +5,11 @@ from list.views import(index,
 TagsListView,
 TagsCreateView,
 TagsUpdateView,
+TagsDeleteView,
+TaskListView,
+TaskCreateView,
+TaskUpdateView,
+TaskDeleteView,
 )
 
 urlpatterns = [
@@ -12,6 +17,11 @@ urlpatterns = [
     path("tags/", TagsListView.as_view(), name="tags"),
     path("tags/create", TagsCreateView.as_view(), name="tags-create"),
     path("tags/<int:pk>/update", TagsUpdateView.as_view(), name="tags-update"),
+    path("tags/<int:pk>/delete", TagsDeleteView.as_view(), name="tags-delete"),
+    path("task/", TaskListView.as_view(), name="task"),
+    path("task/create", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "list"
